62e19c67c324c153a4f5a2db1648c3e39cdc406c
Votre Nom <votre_email@example.com>
2024-05-06 15:16:46 +0200

    delete files

--- app/page.js
+++ app/page.js
@@ -7,11 +7,6 @@ export default function Home() {
       <Header />
       <div class="min-h-lvh bg-cover bg-center bg-no-repeat bg-[url('/images/photo-sf.jpg')] flex items-end justify-center text-center">
         <div class="bg-white/90 w-fit h-min text-5xl p-7 mb-14 rounded-md">08-06-24 </div>
-{/*           <p class=" flex justify-center mt-2 text-3xl">08</p>
-          <p class=" flex justify-center mt-2 text-3xl">-</p>
-          <p class=" flex justify-center mt-2 text-3xl">06</p>
-          <p class=" flex justify-center mt-2 text-3xl">-</p>
-          <p class=" flex justify-center mt-2 text-3xl">24</p> */}
       </div>
     </main>
   );
